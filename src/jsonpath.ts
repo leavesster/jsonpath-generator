229578ff8e5f58cc799ec60b095e9875c0029413
yleaf <11785335+leavesster@users.noreply.github.com>
2024-01-13 11:17:47 +0800

    remove subscription

--- src/jsonpath.ts
+++ src/jsonpath.ts
@@ -4,7 +4,3 @@ export const path = writable("$");
 export const fold = writable(false);
 export const pathList = writable([]);
 export const showPathList = writable(false);
-
-fold.subscribe(fold => {
-    console.log({fold});
-}); 
